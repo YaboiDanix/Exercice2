0ca65096d5115c4e8a407ad201f4e3f53bd26bce
YaboiDanix <Danixz1255@gmail.com>
2026-09-24 20:49:15 +0200

    etape 4

--- main.cpp
+++ main.cpp
@@ -12,17 +12,117 @@ int main() {
     int troll = 80;
     int t = 0;
     while (orc >= 0 && troll >= 0) {
-        int odegats = calculer_degats(14,6);
-
         ++t;
         std::println("tour {}",t);
-        std::println("l'orc frappe le troll : {} degats", odegats);
-        troll -= odegats;
-        std::println("troll a {} PV", troll);
+        int odegats = calculer_degats(14,6);
+        switch (lancer_de(10)) {
+            case 1:
+                std::println("no damage");
+                continue;
+            case 2 :
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 3:
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 4 :
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 5:
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 6 :
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 7:
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 8 :
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 9:
+                std::println("l'orc frappe le troll : {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            case 10 :
+                odegats = calculer_degats(14,6) * 2;
+                std::println("l'orc frappe le troll : coup critique {} degats", odegats);
+                troll -= odegats;
+                std::println("troll a {} PV", troll);
+                break;
+            default :
+                std::println("error");
+                break;
+        }
         int tdegats = calculer_degats(11,4);
-        std::println("le troll frappe l'orc : {}", tdegats);
-        orc -= tdegats;
-        std::println("orc a {} PV", orc);
+        switch (lancer_de(10)) {
+            case 1:
+                std::println("no damage");
+                continue;
+            case 2 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 3 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 4 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 5 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 6 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 7 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 8 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 9 :
+                std::println("le troll frappe l'orc : {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            case 10 :
+                tdegats = calculer_degats(11,4) * 2;
+                std::println("le troll frappe l'orc : coup critique {} degats", tdegats);
+                orc -= tdegats;
+                std::println("orc a {} PV", orc);
+                break;
+            default :
+                std::println("error");
+        }
 
     }
     return 0;
